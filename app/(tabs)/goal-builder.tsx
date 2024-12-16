755a666668c68fc0c6fa5bfa08307013c9bd5e42
JessieWong <jessie.wong@alphasolution.com.mo>
2024-12-16 15:50:20 +0800

    feat: created a form for long term goals

--- app/(tabs)/goal-builder.tsx
+++ app/(tabs)/goal-builder.tsx
@@ -1,3 +1,4 @@
+import BigGoalForm from "@/components/BigGoalForm";
 import React from "react";
 import { View, Text } from "react-native";
 
@@ -5,6 +6,7 @@ export default function GoalBuilderScreen() {
 	return (
 		<View>
 			<Text>Create Goal</Text>
+			<BigGoalForm></BigGoalForm>
 		</View>
 	);
 }
