58456d0231fe45bbfa591687ba6cac7edfc1bbc4
jessie wong <jessie.wong@alphasolution.com.mo>
2024-12-16 20:03:40 +0800

    modified form ui

--- app/(tabs)/goal-builder.tsx
+++ app/(tabs)/goal-builder.tsx
@@ -1,12 +1,25 @@
 import BigGoalForm from "@/components/BigGoalForm";
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, ScrollView } from "react-native";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
 export default function GoalBuilderScreen() {
-	return (
-		<View>
-			<Text>Create Goal</Text>
-			<BigGoalForm></BigGoalForm>
-		</View>
-	);
+  return (
+    <SafeAreaProvider>
+      <SafeAreaView>
+        <ScrollView>
+          <View style={{ margin: 20 }}>
+            <BigGoalForm></BigGoalForm>
+          </View>
+        </ScrollView>
+      </SafeAreaView>
+      {/* <Button
+				icon="camera"
+				mode="contained"
+				onPress={() => console.log("Pressed")}
+			>
+				Press me
+			</Button> */}
+    </SafeAreaProvider>
+  );
 }
--- components/BigGoalForm.tsx
+++ components/BigGoalForm.tsx
@@ -5,105 +5,105 @@ import { Dropdown } from "react-native-paper-dropdown";
 import { DatePickerInput } from "react-native-paper-dates";
 
 export default function BigGoalForm() {
-	const [goalName, setGoalName] = useState("");
-	const [goalDescription, setGoalDescription] = useState("");
-	const [checked, setChecked] = useState("sub-goal");
-	const TRACKING_METHOD_OPTIONS = [
-		{ label: "Completing sub-goals", value: "sub-goal" },
-		{ label: "Completing tasks", value: "task" },
-		{ label: "Manually", value: "manually" },
-	];
+  const [goalName, setGoalName] = useState("");
+  const [goalDescription, setGoalDescription] = useState("");
+  const [checked, setChecked] = useState("sub-goal");
+  const TRACKING_METHOD_OPTIONS = [
+    { label: "Completing sub-goals", value: "sub-goal" },
+    { label: "Completing tasks", value: "task" },
+    { label: "Manually", value: "manually" },
+  ];
 
-	const AREA_OPTIONS = [
-		{ label: "Financial", value: "financial" },
-		{ label: "Health", value: "health" },
-		{ label: "Career", value: "career" },
-		{ label: "Relationship", value: "relationship" },
-		{ label: "Personal Growth", value: "personal-growth" },
-		{ label: "Education", value: "Education" },
-	];
+  const AREA_OPTIONS = [
+    { label: "Financial", value: "financial" },
+    { label: "Health", value: "health" },
+    { label: "Career", value: "career" },
+    { label: "Relationship", value: "relationship" },
+    { label: "Personal Growth", value: "personal-growth" },
+    { label: "Education", value: "Education" },
+  ];
 
-	const [area, setArea] = useState("");
+  const [area, setArea] = useState("");
 
-	const [inputDate, setInputDate] = React.useState(new Date());
-	return (
-		<View style={styles.container}>
-			<View>
-				<Text variant="titleMedium">Goal Name*</Text>
-				<TextInput
-					placeholder="Goal name"
-					value={goalName}
-					onChangeText={(name) => setGoalName(name)}
-				/>
-			</View>
-			<View style={[styles.row, { columnGap: 20 }]}>
-				<View style={{ width: 200 }}>
-					<Text variant="titleMedium">Area*</Text>
-					<Dropdown
-						placeholder="Select Area"
-						options={AREA_OPTIONS}
-						value={area}
-						onSelect={(value) => setArea(value as string)}
-					/>
-				</View>
-				<View>
-					<Text variant="titleMedium">End date*</Text>
-					<DatePickerInput
-						locale="en"
-						value={inputDate}
-						onChange={(d) => setInputDate(d as Date)}
-						inputMode="start"
-						mode="flat"
-						validRange={{ startDate: new Date() }}
-					/>
-				</View>
-			</View>
-			<View>
-				<Text variant="titleMedium">Goal Description*</Text>
-				<TextInput
-					placeholder="Add more detail about this goal"
-					value={goalDescription}
-					onChangeText={(text) => setGoalDescription(text)}
-					multiline={true}
-				/>
-			</View>
-			<View>
-				<Text variant="titleMedium">How will you measure progress*</Text>
-				{TRACKING_METHOD_OPTIONS.map((option, index) => {
-					return (
-						<View style={styles.row} key={index}>
-							<RadioButton
-								value={option.value}
-								status={checked === option.value ? "checked" : "unchecked"}
-								onPress={() => setChecked(option.value)}
-							/>
-							<Text variant="titleMedium">{option.label}</Text>
-						</View>
-					);
-				})}
-			</View>
-			<View style={[styles.row, { justifyContent: "center", columnGap: 30 }]}>
-				<Button mode="outlined" onPress={() => console.log("Pressed candel")}>
-					Cancel
-				</Button>
+  const [inputDate, setInputDate] = React.useState(new Date());
+  return (
+    <View style={styles.container}>
+      <View>
+        <Text variant="titleMedium">Goal Name*</Text>
+        <TextInput
+          placeholder="Goal name"
+          value={goalName}
+          onChangeText={(name) => setGoalName(name)}
+        />
+      </View>
 
-				<Button mode="contained" onPress={() => console.log("Pressed save")}>
-					Save
-				</Button>
-			</View>
-		</View>
-	);
+      <View>
+        <Text variant="titleMedium">Area*</Text>
+        <Dropdown
+          placeholder="Select Area"
+          options={AREA_OPTIONS}
+          value={area}
+          onSelect={(value) => setArea(value as string)}
+        />
+      </View>
+      <View>
+        <Text variant="titleMedium">End date*</Text>
+        <DatePickerInput
+          locale="en"
+          value={inputDate}
+          onChange={(d) => setInputDate(d as Date)}
+          inputMode="start"
+          mode="flat"
+          validRange={{ startDate: new Date() }}
+        />
+      </View>
+
+      <View>
+        <Text variant="titleMedium">Goal Description*</Text>
+        <TextInput
+          placeholder="Add more detail about this goal"
+          value={goalDescription}
+          onChangeText={(text) => setGoalDescription(text)}
+          multiline={true}
+        />
+      </View>
+      <View>
+        <Text variant="titleMedium">How will you measure progress*</Text>
+        {TRACKING_METHOD_OPTIONS.map((option, index) => {
+          return (
+            <View style={styles.row} key={index}>
+              <RadioButton
+                value={option.value}
+                status={checked === option.value ? "checked" : "unchecked"}
+                onPress={() => setChecked(option.value)}
+              />
+              <Text variant="titleMedium">{option.label}</Text>
+            </View>
+          );
+        })}
+      </View>
+      <View style={[styles.row, { justifyContent: "center", columnGap: 30 }]}>
+        <Button mode="outlined" onPress={() => console.log("Pressed candel")}>
+          Cancel
+        </Button>
+
+        <Button mode="contained" onPress={() => console.log("Pressed save")}>
+          Save
+        </Button>
+      </View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		flexDirection: "column",
-		rowGap: 20,
-	},
-	row: {
-		flex: 1,
-		flexDirection: "row",
-		alignItems: "center",
-	},
+  container: {
+    flex: 1,
+    flexDirection: "column",
+    rowGap: 20,
+  },
+  row: {
+    flex: 1,
+    flexDirection: "row",
+    alignItems: "center",
+  },
 });
